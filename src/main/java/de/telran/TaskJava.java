50a2460ab94a67c6b2af8616559c89a29b0aeeaa
rehim <rehim0094@gmail.com>
2023-09-26 15:59:47 +0200

    HomeWork

---
+++ src/main/java/de/telran/TaskJava.java
@@ -0,0 +1,35 @@
+package de.telran;
+
+public class TaskJava {
+    public static void main(String[] args) {
+
+     char myChar = 'G';
+     int myint = 89;
+     byte myByte = 4;
+     short myshort = 56;
+     float myfloat = 4.7333436f;
+     double myDouble = 4.355453532;
+     long myLong = 12121;
+
+     System.out.println("myChar = G");
+     System.out.println("myint = 89");
+     System.out.println("myByte = 4");
+     System.out.println("myshort = 56");
+     System.out.println("myfloat = 4.7333436");
+     System.out.println("myDouble = 4.355453532");
+     System.out.println("myLong = 12121");
+
+    }
+}
+
+/*
+1. В методе main инициализировать все примитивные типы и не примитивные типы.
+        2. Вывести их результат в консоль:
+        char: G
+        int: 89
+        byte: 4
+        short: 56
+        float: 4.7333436
+        double: 4.355453532
+        long: 12121
+*/
