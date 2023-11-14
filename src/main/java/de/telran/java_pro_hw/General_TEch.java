50da808297ae4dcdea717ff615367783c62233e6
rehim <rehim0094@gmail.com>
2023-11-14 23:12:09 +0100

    HomeWork 6

---
+++ src/main/java/de/telran/java_pro_hw/General_TEch.java
@@ -0,0 +1,28 @@
+package de.telran.java_pro_hw;
+
+public class General_TEch {
+
+  /*  Приведите десять типовых бизнес-процессов для предметной области
+        ВИДЕО-ХОСТИНГ (Youtube), а также их соответствие по CRUD.*/
+
+    // 1.Регистрация клиента (Create)
+    // 2.Создание канала (Create)
+    // 3.Удаление канала (Update)
+    // 4.Поиск видео (Read)
+    // 5.Подписаться на канал (Create)
+    // 6.Отписаться из канала (Update)
+    // 7.История просмотров (Read)
+    // 8.Удаление комментов (Delete)
+    // 9.Изменение имени канала (Update)
+    // 10.Скачать видео (Read)
+
+   /* Вывести название и стоимость товаров от 20 EUR.
+ Вывести страны поставщиков.
+ Вывести контактные имена клиентов, кроме тех, что из France и USA.*/
+
+    //SELECT ProductName, Price FROM products WHERE price > 20;
+    // SELECT SupplierName,Country FROM Suppliers;
+//SELECT ContactName,Country FROM Customers WHERE Country NOT IN ('France', 'USA');
+
+
+}
